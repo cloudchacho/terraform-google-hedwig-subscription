cb00a7a528642f69c1bc5eee5b6d152b99c42de2
Aniruddha Maru <maru@standard.ai>
2021-02-22 17:47:28 -0800

    Better handling for empty iam_service_account (#7)
    Terraform can't evaluate plan if count depends on other resources. To allow for cases like this, don't
    use count for IAM

--- main.tf
+++ main.tf
@@ -32,25 +32,25 @@ resource "google_pubsub_subscription" "subscription" {
 
 data "google_iam_policy" "subscription_policy" {
   binding {
-    members = [
-      "serviceAccount:${var.iam_service_account}",
-      "serviceAccount:service-${data.google_project.current.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
-    ]
+    members = concat(
+      var.iam_service_account == "" ? [] :
+      ["serviceAccount:${var.iam_service_account}"],
+      ["serviceAccount:service-${data.google_project.current.number}@gcp-sa-pubsub.iam.gserviceaccount.com"]
+    )
     role = "roles/pubsub.subscriber"
   }
 
   binding {
-    members = [
-      "serviceAccount:${var.iam_service_account}",
-      "serviceAccount:service-${data.google_project.current.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
-    ]
+    members = concat(
+      var.iam_service_account == "" ? [] :
+      ["serviceAccount:${var.iam_service_account}"],
+      ["serviceAccount:service-${data.google_project.current.number}@gcp-sa-pubsub.iam.gserviceaccount.com"]
+    )
     role = "roles/pubsub.viewer"
   }
 }
 
 resource "google_pubsub_subscription_iam_policy" "subscription_policy" {
-  count = var.iam_service_account == "" ? 0 : 1
-
   policy_data  = data.google_iam_policy.subscription_policy.policy_data
   subscription = google_pubsub_subscription.subscription.name
 }
